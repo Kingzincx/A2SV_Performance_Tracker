078df8a219ff035102467340e6fc65323450b5ef
Kingzincx <joelvidj@gmail.com>
2026-02-06 23:20:19 +0100

    array subset of another array

---
+++ array_subset_of_another_array.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        freq = {}
+        for x in a:
+            freq[x] = freq.get(x, 0) + 1
+        for x in b:
+            if freq.get(x, 0) <= 0:
+                return False
+            freq[x] -= 1
+        return True
