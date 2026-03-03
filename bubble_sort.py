1783403f0f09383edd089310af5385c95c294f27
Kingzincx <joelvidj@gmail.com>
2026-03-03 21:37:59 +0100

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+def countSwaps(a):
+    numSwaps = 0
+    n = len(a)
+
+    for i in range(n):
+        for j in range(n - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                numSwaps += 1
+
+    print("Array is sorted in", numSwaps,"swaps.")
+    print("First Element:",a[0])
+    print("Last Element:",a[-1])
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
